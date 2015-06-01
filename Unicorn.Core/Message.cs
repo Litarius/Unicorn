2491496d67faafff0c6d7cc94341d13ea197458a
Litarius <artemshmarlovsky@gmail.com>
2015-06-01 22:04:22 +0400

    1

--- Unicorn.Core/Message.cs
+++ Unicorn.Core/Message.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unicorn.Server
+namespace Unicorn.Core
 {
-    public class Class1
+    class Message
     {
     }
 }
--- Unicorn.Server/Listener.cs
+++ Unicorn.Server/Listener.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unicorn.Core
+namespace Unicorn.Server
 {
-    public class Class1
+    public class Listener
     {
     }
 }
